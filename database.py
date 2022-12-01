db3b2036b424b547624bf9c8232209274f649af2
joshmysore <joshmysore@college.harvard.edu>
2022-12-01 18:39:54 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,13 @@
 import cs50
 import csv
 
-cs50.get_float()
-
 # db = SQL("sqlite:///quizzify.db")
 # print(db.execute("SELECT * FROM Users;"))
 
 print("START")
 with open("top10s.csv") as f:
         reader = csv.reader(f)
-        headers = next(reader)
-        headers.pop(0)
+        # headers = reader.next()
+        # headers.pop(0)
         for row in reader:
             print(row)
