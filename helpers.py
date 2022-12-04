e272c2dd25dbe1b149841a22fecaf04b9f30c3c7
joshmysore <joshmysore@college.harvard.edu>
2022-12-04 17:12:30 -0500

    changes

--- helpers.py
+++ helpers.py
@@ -1,8 +1,7 @@
 import os
-import requests
 import urllib.parse
 
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, session
 from functools import wraps
 
 
@@ -19,6 +18,20 @@ def apology(message, code=400):
             s = s.replace(old, new)
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
+
+def apology2(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology2.html", top=code, bottom=escape(message)), code
 
 
 def login_required(f):
