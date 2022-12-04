4ceb958a78133bf1b3ea89d03435683c44ea3b65
calderonsa21 <scalderon@college.harvard.edu>
2022-12-03 19:27:06 -0500

    x

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+# Configure CS50 Library to use SQLite database 
 db = SQL("sqlite:///quizzify.db")
 
 
